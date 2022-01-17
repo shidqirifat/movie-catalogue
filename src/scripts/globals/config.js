14b9cc38255f52e93b544496337888405731843a
shidqirifat <rifatpangestu87@gmail.com>
2022-01-17 21:29:33 +0700

    first commit: set up project, cache api, service worker, etc.

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  KEY: 'e8d176dd823a48c48156d92005fbb90d',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'MovieCatalogue-V1',
+  // CACHE_NAME: new Date().toISOString(),
+};
+
+export default CONFIG;
---
+++ src/scripts/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-console */
+import 'regenerator-runtime';
+import '../styles/style.css';
+import '../styles/responsive.css';
+import App from './views/app';
+import swRegister from './utils/sw-register';
+
+const app = new App({
+  button: document.getElementById('hamburgerButton'),
+  drawer: document.getElementById('navigationDrawer'),
+  content: document.getElementById('mainContent'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+  swRegister();
+});
