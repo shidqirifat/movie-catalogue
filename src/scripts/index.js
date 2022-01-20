36724deb5d54940b3c4ee3be6bef12abd264d2cd
shidqirifat <rifatpangestu87@gmail.com>
2022-01-20 23:51:50 +0700

    add websocket and push notification

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,8 @@ import '../styles/style.css';
 import '../styles/responsive.css';
 import App from './views/app';
 import swRegister from './utils/sw-register';
+import CONFIG from './globals/config';
+import WebSocketInitiator from './utils/websocket-initiator';
 
 const app = new App({
   button: document.getElementById('hamburgerButton'),
@@ -18,4 +20,5 @@ window.addEventListener('hashchange', () => {
 window.addEventListener('load', () => {
   app.renderPage();
   swRegister();
+  WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
 });
